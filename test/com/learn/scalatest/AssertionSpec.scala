db6efe1764397b638baa97b47fe7f1f818a02ffc
gongwei <gongwei.zhang@autodesk.com>
2014-07-13 11:31:11 +0800

    add assertions usage

--- test/com/learn/scalatest/AssertionSpec.scala
+++ test/com/learn/scalatest/AssertionSpec.scala
@@ -5,5 +5,17 @@ package com.learn.scalatest
  * Date: 7/13/2014.
  */
 class AssertionSpec extends UnitSpec{
+  "5 - 2 == 2" should "fail" in {
+    assume(false)
 
+    val a = 5
+    val b = 2
+    assertResult(2) {
+      a - b
+    }
+  }
+
+  ignore should "not run after tagging ignore" in {
+
+  }
 }
