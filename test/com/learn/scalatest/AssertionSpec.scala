aeaaf35993275a49ee938549c3307a01b44a6e86
gongwei <gongwei.zhang@autodesk.com>
2014-07-13 11:13:01 +0800

    Finish StackSpec

---
+++ test/com/learn/scalatest/AssertionSpec.scala
@@ -0,0 +1,9 @@
+package com.learn.scalatest
+
+/**
+ * Author: zhanggo
+ * Date: 7/13/2014.
+ */
+class AssertionSpec extends UnitSpec{
+
+}
--- test/com/learn/scalatest/StackSpec.scala
+++ test/com/learn/scalatest/StackSpec.scala
@@ -5,11 +5,17 @@ import collection.mutable.Stack
 class StackSpec extends UnitSpec{
 
   "A Stack" should "pop values in first-in-last-out order" in {
-
     val stack = new Stack[Int]
     stack.push(1)
     stack.push(2)
     assert(stack.pop() == 2)
     assert(stack.pop() == 1)
+  }
+
+  it should "throw NoSuchElementException if an empty stack is pop" in {
+    val emptyStack = new Stack[String]
+    intercept[NoSuchElementException] {
+      emptyStack.pop()
+    }
   }
 }
