33ff06e1c7411f1d811471c5726bebb21b115a7a
gongwei <gongwei.zhang@autodesk.com>
2014-07-28 10:24:33 +0800

    create stub for http interface

---
+++ app/controllers/http/Products.scala
@@ -0,0 +1,22 @@
+package controllers.http
+
+import play.api.mvc.{Action, Controller}
+
+object Products extends Controller{
+
+  def list(pageNumber: Int) = Action {
+    NotImplemented
+  }
+
+  def details(ean: Long) = Action {
+    NotImplemented
+  }
+
+  def edit(ean: Long) = Action {
+    NotImplemented
+  }
+
+  def update(ean: Long) = Action {
+    NotImplemented
+  }
+}
