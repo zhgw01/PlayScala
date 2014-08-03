08077d226d8448d551474e5d76f4991c12114737
Gongwei Zhang <zhgw01@gmail.com>
2014-08-03 16:53:12 +0800

    multiple async service

--- app/controllers/Proxy.scala
+++ app/controllers/Proxy.scala
@@ -4,10 +4,12 @@ package controllers
  * Created by galway on 7/30/14.
  */
 
+import play.api.libs.json.Json
 import play.api.libs.ws._
 import play.api.Play.current
 import play.api.mvc._
 import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.Future
 
 object Proxy extends Controller{
 
@@ -17,5 +19,22 @@ object Proxy extends Controller{
         .get().map {
         case res => Ok(res.body).as(res.allHeaders("Content-Type")(0))
       }
+  }
+
+  def latency = Action.async {
+    val start = System.currentTimeMillis();
+
+    def getLatency(r: Any): Long = System.currentTimeMillis() - start
+
+    val yahooTime = WS.url("http://www.yahoo.com").get().map(getLatency)
+    val bingTime = WS.url("http://www.bing.com").get().map(getLatency)
+
+    Future.sequence(Seq(yahooTime, bingTime)).map {
+      case times => Ok(Json.toJson(
+        Map("yahoo" -> times(0), "bing" -> times(1))
+      ))
+    }
+
+
   }
 }
